9e98bad358f2c5a503775e40265f5dafa782d289
unknown <dgetskova@gmail.com>
2015-07-23 17:20:04 +0300

    stack

---
+++ stack/stack/main.cpp
@@ -0,0 +1,29 @@
+#include "stack.h"
+#include <typeinfo>
+using namespace std;
+
+int main()
+{
+	int a = 9;
+	typeid(a);	
+	elem<int> element1(1);
+	elem<int> element2(2);
+	elem<int> element3(3);
+	elem<int> elementCopy(element1);
+
+	Stack<int> stack1(element1);
+	stack1.push(element2);
+	stack1.push(element3);
+
+	Stack<int> stack2(stack1);
+	cout << "a";
+	stack2.push(element1);
+	stack2.push(element1);
+	stack1.push(element2);
+
+	stack1 = stack2;
+
+	stack2.pop();
+	elem<int> fromPeek(stack2.peek());
+
+}
